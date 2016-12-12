83074524ea68723f000bcd23fabc311cb65cbfd0
Andrew Berkeley <andrew.berkeley@sepa.org.uk>
2016-12-12 11:51:34 +0000

    add semi colon

--- lib/+PackageManager/+LocalPackage/all2Path.m
+++ lib/+PackageManager/+LocalPackage/all2Path.m
@@ -1,7 +1,7 @@
 function [ output_args ] = all2Path
     list = PackageManager.LocalPackage.list;
 
-    addpath(PackageManager.Install.rootPath)
+    addpath(PackageManager.Install.rootPath);
 
     for e = 1:length(list)
         addpath([PackageManager.Install.rootPath, '\', list{e}]);
